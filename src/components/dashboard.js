cdbe7748be88ef3d06f108aaae4b4932f25037af
Changiz Hosseini <zyddal1310@gmail.com>
2018-03-20 15:48:10 +0100

    added expense summary component

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './expense-list';
 import ExpenseListFilters from './expense-list-filters';
+import ExpensesSummary from './expenses-summary';
 
 const Dashboard = () => (
   <div>
+    <ExpensesSummary />
     <ExpenseListFilters />
     <ExpenseList />
   </div>
