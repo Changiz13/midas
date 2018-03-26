0878dbf9539cabe4155faeb65a7271b513678757
Changiz Hosseini <zyddal1310@gmail.com>
2018-03-26 22:28:28 +0200

    recieves firebases expenses on index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/app-router';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
@@ -18,4 +18,8 @@ const JSX = (
   </Provider>
 );
 
-ReactDOM.render(JSX, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(JSX, document.getElementById('app'));
+});
